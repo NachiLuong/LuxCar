3c7ab94b0aebe64a28700e0f46b718c2ad58facb
doquocviet-sept3rd <vcoder@outlook.com.vn>
2022-05-02 11:15:46 +0700

    annimation main and login

---
+++ app/src/main/java/com/luxcar/activities/login.java
@@ -0,0 +1,16 @@
+package com.luxcar.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.luxcar.R;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
